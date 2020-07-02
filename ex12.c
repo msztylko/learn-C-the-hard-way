872916c59c6c31bdbfee5fb9cc5c1e71a46aea72
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-07-02 11:36:00 +0200

    fixes

--- ex12.c
+++ ex12.c
@@ -6,13 +6,13 @@ int main(int argc, char *argv[])
 	char name[] = "Zed";
 	char full_name[] = {
 		'Z', 'e', 'd',
-		' ', 'A', '.',
+		' ', 'A', '.', ' ',
 		'S', 'h', 'a', 'w', '\0'
 	};
 
 	// WARNING: On some systems you may have to change the 
 	// %ld in this code to a %u since it will use unsigned ints
-	printf("The size of an int: $ld\n", sizeof(int));
+	printf("The size of an int: %ld\n", sizeof(int));
 	printf("The size of areas (int[]): %ld\n", sizeof(areas));
 	printf("The number of ints in areas %ld\n",
 			sizeof(areas) / sizeof(int));
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 
 	printf("The size of full_name (char[]): %ld\n", sizeof(full_name));
 	printf("The number of chars: %ld\n", sizeof(full_name) / sizeof(char));
-	pritnf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
+	printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
 	
 	return 0;
 }

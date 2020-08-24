f3bcc02babc2068aba51b05a43b540dfda3f916c
Marcin Sztylko <marcin.sztylko@gmail.com>
2020-08-24 17:04:57 +0200

    copy string

---
+++ copy-string.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Array verion
+
+// void copyString(char to[], char from[])
+// {
+//     int i;
+//     
+//     for (i = 0; from[i] != '\0'; ++i)
+//         to[i] = from[i];
+// 
+//     to[i] = '\0';
+// }
+
+// Pointer version
+
+void copyString(char *to, char *from)
+{
+    for ( ; *from != '\0'; ++from, ++to)
+        *to = *from;
+
+    *to = '\0';
+}
+
+int main(void)
+{
+    void copyString(char *to, char *from);
+    char string1[] = "A string to be copied.";
+    char string2[50];
+
+    copyString(string2, string1);
+    printf("%s\n", string2);
+
+    copyString(string2, "So is this.");
+    printf("%s\n", string2);
+
+    return 0;
+}
